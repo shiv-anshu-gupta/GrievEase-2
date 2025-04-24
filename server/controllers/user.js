32558d55e9b9ca425ad97066408ccb59e15bc3ba
unknown <shivanshu2019gupta@gmail.com>
2025-04-25 03:39:54 +0530

    modify for production

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const serverUrl = process.env.serverUrl;
 export const registerUser = async (req, res) => {
   try {
     const { fullName, email, password, role } = req.body;
